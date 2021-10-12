7c5fcb3a5805e7b7f231c00b4cfcd967bbcb8bd8
dawidzbizek <92389823+dawidzbizek@users.noreply.github.com>
2021-10-12 21:29:29 +0200

    finish commit

---
+++ task-2.php
@@ -0,0 +1,45 @@
+<?php
+const maxElementsOfVersion = 4;
+
+function decodeHumanVersion(string $humanVersion) : int
+{
+    $version = [];
+    $decodedVersion = explode('.', str_replace(['a', 'b'], '', $humanVersion));
+
+    for($i = 0; $i < maxElementsOfVersion; $i++)
+    {
+        $rawValue = $decodedVersion[$i] ?? '0';
+        $tmpValue = str_split($rawValue);
+        $value = [];
+
+        for($j = 0; $j < 3; $j++)
+            $value[] = $tmpValue[$j] ?? '0';
+
+        $version[] = implode('', $value);
+    }
+
+    if(substr($humanVersion, -1) == 'a')
+        $version[3] = 1;
+    elseif(substr($humanVersion, -1) == 'b')
+        $version[3] = 2;
+    else
+        $version[3] = 3;
+
+    return (int) implode('', $version);
+}
+
+function compareVersions(string $a = '0', string $b = '0') : string
+{
+    $decodedA = decodeHumanVersion($a);
+    $decodedB = decodeHumanVersion($b);
+
+    if($decodedA == $decodedB)
+        return 'same';
+    elseif($decodedA > $decodedB)
+        return "$a is bigger";
+    else
+        return "$b is bigger";
+}
+
+echo compareVersions('5.3', '5.3.4a');
+?>
